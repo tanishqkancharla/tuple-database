52ab1214340af6b449b3a729fc118e50c4c64bba
Tanishq Kancharla <tanishqkancharla3@gmail.com>
2022-07-28 18:04:42 -0400

    failing test

--- src/database/sync/subscribeQuery.test.ts
+++ src/database/sync/subscribeQuery.test.ts
@@ -38,4 +38,68 @@ describe("subscribeQuery", () => {
 
 		destroy()
 	})
+
+	it.skip("doesn't run second callback if it is destroyed in first", async () => {
+		type Schema =
+			| {
+					key: ["filesById", number]
+					value: string
+			  }
+			| {
+					key: ["focusedFileId"]
+					value: number
+			  }
+
+		const db = new TupleDatabaseClient<Schema>(
+			new TupleDatabase(new InMemoryTupleStorage())
+		)
+
+		const initTx = db.transact()
+		initTx.set(["filesById", 1], "file 1 value")
+		initTx.set(["focusedFileId"], 1)
+		initTx.commit()
+
+		let focusedFile: number | undefined = undefined
+		let focusedFileValue: string | undefined = undefined
+		let subscription:
+			| { result: string | undefined; destroy: () => void }
+			| undefined = undefined
+
+		function subscribeToFocusedFile(focusedFile: number) {
+			subscription = subscribeQuery(
+				db,
+				(db) => db.get(["filesById", focusedFile]),
+				(value) => {
+					focusedFileValue = value
+				}
+			)
+
+			focusedFileValue = subscription.result
+		}
+
+		const focusedFileQuery = subscribeQuery(
+			db,
+			(db) => db.get(["focusedFileId"])!,
+			(result) => {
+				focusedFile = result
+				subscription?.destroy()
+				subscribeToFocusedFile(focusedFile)
+			}
+		)
+
+		focusedFile = focusedFileQuery.result
+		subscribeToFocusedFile(focusedFile)
+
+		assertEqual(focusedFile, 1)
+		assertEqual(focusedFileValue, "file 1 value")
+
+		const tx = db.transact()
+		tx.remove(["filesById", 1])
+		tx.set(["filesById", 2], "file 2 value")
+		tx.set(["focusedFileId"], 2)
+		tx.commit()
+
+		assertEqual(focusedFile, 2)
+		assertEqual(focusedFileValue, "file 2 value")
+	})
 })
